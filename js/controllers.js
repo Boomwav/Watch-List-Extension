67bbbe8f2771d04607be37cd363e1894082894e5
Simon Dufour <sim.dufour@gmail.com>
2014-02-16 16:37:55 -0500

    Changed the grid so that it really uses YouTube data. I also found how to link to channels with a channelID. My next step should be the riskier one. Saving data somewhere to use later. If I can do that, I could build my whole application. I'd search for the playlist ID from the URL. I could then use its ID to get more information from it.

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,15 @@
 var watchListApp = angular.module('watchListApp', []);
  
-watchListApp.controller('PlaylistsCtrl', function ($scope) {
-  $scope.playlists = [
-    {'title': 'Civilization 5: France (Tourism)',
-     'channelUrl': 'http://www.youtube.com/user/quill18',
-     'channelName': 'quill18'},
-    {'title': 'EU IV (Co op MP) - Sunni invasion of Europe',
-     'channelUrl': 'http://www.youtube.com/user/SBFMadDjinn',
-     'channelName': 'SBFMadDjinn'},
-    {'title': 'Xcom Enemy Within: Impossible Ironman South America',
-     'channelUrl': 'http://www.youtube.com/user/SBFMadDjinn',
-     'channelName': 'SBFMadDjinn'},
-  ];
+watchListApp.controller('PlaylistsCtrl', function ($scope, $http) {
+    var dataurl = "https://www.googleapis.com/youtube/v3/playlists?part=id,snippet&channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUvP3-ZZ_zLOY2eMODHbNrDKR0Mwd20r4";
+
+    $http.get(dataurl).success(function(data){
+        $scope.playlists = data.items;
+    });
 });
+
+//watchListApp.filter('nospace', function () {
+//    return function (value) {
+//        return (!value) ? '' : value.replace(/ /g, '');
+//    };
+//});
