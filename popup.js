0098b67b0c802d5097041646b4334899a133e6cb
Simon Dufour <sim.dufour@gmail.com>
2014-02-16 13:43:10 -0500

    Finally got my YouTube API key to work. Now, I can start searching for Playlists or get information on a playlist currently showed. I have to dig through the YouTube api but it should be all good. Awesome.

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
 chrome.tabs.getSelected(null, function(tab) {
     document.getElementById('currentLink').innerHTML = tab.url;
 });
+
+// chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+//   // Use the token.
+// });
+$( function () { 
+  $("#search-button").click(function() {
+      alert("test");
+     $.get(
+      "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUvP3-ZZ_zLOY2eMODHbNrDKR0Mwd20r4",
+      {},
+      function(data) {
+         var str = JSON.stringify(data);
+        $('#search-container').html('<pre>' + str + '</pre>');
+      }
+    );  
+  });
+});
