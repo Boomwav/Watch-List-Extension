87de44f2e63024101651103535f063b3a8eb2fc0
Simon Dufour <sim.dufour@gmail.com>
2014-02-20 13:08:30 -0500

    I added playlist details to the channel details controller. Oops. Next part should be building up the different views to get an idea of what it should look like. I have to keep a close eye on the API usage too. Especially once I release it publicly. Maybe adding indexDB caching might make things less porky on API calls. The user watch list could become a lot of API calls. I'd still need to check if a channel changed or if a playlist changed from time to time so I'll need to keep a list somewhere at some point.

--- js/controllers.js
+++ js/controllers.js
@@ -11,17 +11,15 @@ watchlistControllers.controller('ChannelLibraryCtrl', ['$scope', '$http',
     }
 ]);
 
-watchlistControllers.controller('ChannelDetailCtrl', ['$scope', '$routeParams',
-    function ($scope, $routeParams) {
+watchlistControllers.controller('ChannelDetailCtrl', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
         $scope.channelId = $routeParams.channelId;
+		
+		var dataurl =  "https://www.googleapis.com/youtube/v3/playlistItems?part=id%2Csnippet%2CcontentDetails%2Cstatus&playlistId=PLOU2XLYxmsII2vIhzAyW6eouf62ur2Z2q&key=AIzaSyCUvP3-ZZ_zLOY2eMODHbNrDKR0Mwd20r4";
+		
+		$http.get(dataurl).success(function(data){
+            $scope.playlists = data.items;
+            $scope.result = JSON.stringify(data);
+        });
     }
 ]);
-
-
-
-
-//watchListApp.filter('nospace', function () {
-//    return function (value) {
-//        return (!value) ? '' : value.replace(/ /g, '');
-//    };
-//});
